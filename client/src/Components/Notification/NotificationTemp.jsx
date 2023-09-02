e1c4ce25276e487e1a7ac10d8dd7156fa8583567
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-09-02 10:38:00 +0530

    improved code

--- client/src/Components/Notification/NotificationTemp.jsx
+++ client/src/Components/Notification/NotificationTemp.jsx
@@ -3,12 +3,12 @@ import Speaker from "./speaker2.jpg"
 
 const NotificationTemp = (props)=>{
     return (
-        <div className='w-2/3 mt-2 h-24  mx-auto content-center drop-shadow-md flex flex-row items-center justify-center bg-white rounded-2xl px-8 py-4'>
-          {/* <h1 className='text-2xl font-bold text-[#130223]'>IIT BHU Community</h1> */}
-          <img src={Speaker} className="w-10 mr-12"/>
+        <div className='w-full mt-3 mx-auto content-center drop-shadow-md flex flex-col items-start bg-white rounded-2xl py-4 pb-5 pr-5'>
+          <h1 className="px-10 mb-5 text-xl font-bold">{props.title}</h1>  
+          <div className="flex flex-row items-center">
+          <img src={Speaker} className="w-10 mr-12 mx-10"/>
           <p className='text-sm'>{props.message}</p>
-          {/* <p className='text-orange-500 text-xs font-medium'>Members: 9047</p> */}
-          {/* <button className='py-2 px-4 w-36 bg-purple-700 text-white font-medium rounded-full'>Join in</button> */}
+          </div>
       </div>
     )
 }
