e94b86c280d0a80f700cbb0e4c9de64aaa16a47f
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Air-2.local>
2023-09-19 02:42:08 +0530

    Transport Card Updated

--- client/src/Pages/Transport.jsx
+++ client/src/Pages/Transport.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import CommunityCard from '../Components/Community/CommunityCard'
+import Tab from '../Components/Community/Tab'
+import Navbar from '../Components/Navbar'
+import Topbar from '../Components/Topbar'
+import TransportCard from '../Components/Transport/Card'
+import TransportInfo from '../Components/Transport/TransportInfo'
 
 const Transport = () => {
   return (
-    <div>Transport</div>
+    <div className='flex flex-row'>
+      <Navbar />
+      <div className='flex flex-col w-[100vw] h-[100vh]'>
+        <div>
+          <Topbar />
+        </div>
+
+        <div>
+          <TransportCard />
+          <TransportCard />
+          <TransportCard />
+          <TransportCard />
+          <TransportCard />
+        </div>
+      </div>
+      <div>
+        <TransportInfo/>
+      </div>
+    </div>
   )
 }
 
